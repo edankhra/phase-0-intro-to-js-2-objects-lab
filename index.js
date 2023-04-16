6b904fd0cd06fc4ceb8e331b18a25673a9b81c01
Ekta <edankhra@gmail.com>
2023-04-16 14:41:17 -0400

    completed

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-let employee = {
+/*let employee = {
+    name: "Bob patel",
+    streetAddress: "90,arial st,toronto"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newobj = {...employee};
+    newobj[key] = value;
+    return newobj;
+   
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }*/
+
+  let employee = {
     name: "Bob patel",
     streetAddress: "90,arial st,toronto"
   };
@@ -25,7 +53,6 @@ let employee = {
     delete employee[key];
     return employee;
   }
-
   
   
   
